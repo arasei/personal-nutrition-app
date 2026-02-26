14e814adb62e132a52413af4d3f11366f1c8e96f
arasei <arasei1019@gmail.com>
2026-02-26 14:54:53 +0900

    診断回答保存ロジック(仮)

---
+++ web/app/api/diagnosis/complete/route.ts
@@ -0,0 +1,70 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { userId, answers } = body
+
+    const result = await prisma.$transaction(async (tx) => {
+      //Diagnosis(1つの履歴箱)作成
+      const diagnosis = await tx.diagnosis.create({
+        data: {
+          userId,
+          status: "COMPLETED",
+          completedAt: new Date()
+        }
+      })
+
+      //回答をフロント形式からDB保存形式へ変換して一括保存(Answerを保存)
+      await tx.diagnosisAnswer.createMany({
+        data: answers.map((a: any) => ({
+          diagnosisId: diagnosis.id,
+          questionId: a.questionId,
+          value: a.value,
+          answeredAt: new Date()
+        }))
+      })
+
+      //スコア計算
+
+      //スコア集計箱を作成
+      const scoreMap: Record<string, number> = {}
+
+      //栄養素ごとに合計点をループで集計
+      for (const a of answers) {
+        if (!scoreMap[a.nutrient]) {
+          scoreMap[a.nutrient] = 0
+        }
+        scoreMap[a.nutrient] += a.value
+      }
+
+      //栄養素ごとの合計スコアが入ったscoreMapをもとに、点数が高い順にrankingを作成
+      //rankingは表示用に整形されたデータ
+      const ranking = Object.entries(scoreMap)
+        .map(([nutrient, total]) => ({ nutrient, total }))
+        .sort((a, b) => b.total - a.total)
+
+        //スコア保存(複数の栄養素スコアを一括保存)
+        //DB保存用にrankingをDBのカラム名に合わせて再整形してる
+        await tx.diagnosisNutrientScore.createMany({
+          data: ranking.map((r) => ({
+            diagnosisId: diagnosis.id,
+            nutrient: r.nutrient,
+            score: r.total
+          }))
+        })
+
+        return {
+          diagnosisId: diagnosis.id,
+          ranking
+        }
+    })
+
+    return Response.json(result)
+  } catch (error) {
+    console.error(error)
+    return Response.json({ error: "保存に失敗しました" }, { status: 500 })
+  }
+}
